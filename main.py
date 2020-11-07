7fe1f65d1cfb58fb55543ba8265bb964fdcb216d
Ohad <ohadohad5@gmail.com>
2020-11-07 13:54:59 +0200

    commit 4

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from bs4 import BeautifulSoup as soup
+from bs4 import BeautifulSoup
 import requests
 import urllib.request
 import csv
 from urllib.request import urlopen
+# import stock_performance as sp
+
 
 def url_check(url):
     """
     Connecting and checking request status to url address
     """
-
     # getting url request
-    print('Connecting to ',url)
+    print('Connecting to ', url)
     req_check = requests.get(url)
 
     # checking request status
@@ -24,7 +25,8 @@ def url_check(url):
 
     return
 
-def write_file(url_name,rows):
+
+def write_file(url_name, rows):
     """
     Create csv named "url_name".csv and write rows to the file
     """
@@ -32,89 +34,78 @@ def write_file(url_name,rows):
 
     print('\nWriting results to file..')
     try:
-        with open(output_file_name,'w', newline='') as f_output:
+        with open(output_file_name, 'w', newline='') as f_output:
             csv_output = csv.writer(f_output)
             csv_output.writerows(rows)
-            print('file "{}" created suuccesfuly\n\n'.format(output_file_name))
+            print('file "{}" created suuccesfuly\n'.format(output_file_name))
     except:
         print('Error writing to file "{}" '.format(output_file_name))
         return
 
 
-
-
-url_address1 = "https://www.marketwatch.com/tools/screener?mod=stocks"  ### Top gainers
-
-url_address2= "https://www.marketwatch.com/tools/screener?exchange=Nyse&report=MostActive"  ### Most Active
-
-url_dict = {'Top gainers':"https://www.marketwatch.com/tools/screener?mod=stocks",
-            'Most Active':"https://www.marketwatch.com/tools/screener?exchange=Nyse&report=MostActive"
-            }
-
-
-for url_name, url_address in url_dict.items():  ## itrating through the url pages in the url_dict
-
-    url_check(url_address)
-
-
-    # query the website and return the html to the variable 'page'
-    page = requests.get(url_address)
-    # page = urlopen(url_address)
-
-    # parse the html using beautiful soup and store in variable 'soup'
-    soup = soup(page.content, 'html.parser')
-
-    # find results within table (table_body)
-    # table_body = soup.find_all('tbody')
-
-    ########
-    table = soup.find('table')
-
-    if table is not None and len(table.find_all('tr'))>0:
-        table_elements = table.find_all('tr')
-    ########
-
-    # table_elements = soup.find_all('tr')
-    # print('Number of elements', (len(table_elements)-1))
-
-    # create and write headers to a list
-    rows = []
-    rows.append(['Symbol ', 'Company', 'Last',	'Change','%_Change','Volume','$_Traded', 'Description', 'Staff', 'Comments'])
-
-    for result in table_elements:
-        try:
-            # find all columns per result
-            data = result.find_all('td')
-            # check that columns have data
-            if len(data) == 0:
-                continue
-
-            # write columns to variables
-            Symbol = data[0].getText()
-            company = data[1].getText()
-            Last = data[2].getText()
-            Change = data[3].getText()
-            Change_percent = data[4].getText()
-            Volume = data[5].getText()
-            Traded = data[6].getText()
-
-            # write each result to rows
-            rows.append([Symbol, company, Last, Change, Change_percent, Volume, Traded])
-        except:
-            pass
-
-    print(rows)
-
-    # Create csv and write rows to output file
-    write_file(url_name,rows)
-
-
-
-
-
+###### program start here ###########
 def main():
+    base_url = "https://www.marketwatch.com"
 
-    return
+    url_dict = {'Top gainers': "https://www.marketwatch.com/tools/screener?mod=stocks",
+                'Most Active': "https://www.marketwatch.com/tools/screener?exchange=Nyse&report=MostActive",
+                }
+
+    stocks_symbol_list = []
+    stocks_links_list = []
+
+    for name, url in url_dict.items():
+        url_check(url)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        # member = soup.find_all("span", {"class":"current"})
+        table = soup.find('table')
+
+        #
+        if table is not None and len(table.find_all('tr')) > 0:
+            table_elements = table.find_all('tr')
+
+        rows = []
+        rows.append(
+            ['Symbol ', 'Company', 'Last', 'Change', '%_Change', 'Volume', '$_Traded', 'Description', 'Staff', 'Comments'])
+
+        for result in table_elements:
+            try:
+                # find all columns per result
+                data = result.find_all('td')
+                # check that columns have data
+                if len(data) == 0:
+                    continue
+
+                # write columns to variables
+                symbol = data[0].getText()
+                company = data[1].getText()
+                last = data[2].getText()
+                change = data[3].getText()
+                change_percent = data[4].getText()
+                volume = data[5].getText()
+                traded = data[6].getText()
+
+                if symbol not in stocks_symbol_list: # adding the stock symbol to a list
+                    stocks_symbol_list.append(symbol)
+                    print('{} added to stocks_symbol_list'.format(symbol))
+
+                rows.append([symbol, company, last, change, change_percent, volume,traded]) # write each result to rows
+            except:
+                pass
+
+            link = result.find("a").get('href') # getting the stock pages url link
+
+            if [base_url + link] not in stocks_links_list:  # adding the stock page url link to a list
+                stocks_links_list.append([symbol,base_url + link])
+
+        print(rows)
+        write_file(name, rows)
+
+    write_file('stocks_links_list', stocks_links_list)
 
 if __name__ == '__main__':
     main()
+    # sp.main()
+
+
---
+++ stock_performance.py
@@ -0,0 +1,105 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+def file_to_list(filename):
+    """
+    receive a file name and return a list with the file's links
+    """
+    stocks_links_list = []
+
+    with open(filename, 'r') as file:
+        csvfile = csv.reader(file)
+        if csvfile:
+            for i, lines in enumerate(csvfile):
+                if len(lines) < 1:
+                    print("line {} error -> line skipped".format(i))
+
+                elif (''.join(lines).strip()):
+                    stocks_links_list.append([lines[0], lines[1]])
+        else:
+            print('Error - empty file')
+            exit()
+
+    return stocks_links_list
+
+def url_check(url):
+    """
+    Connecting and checking request status to url address
+    """
+
+    # getting url request
+    print('Connecting to ', url)
+    req_check = requests.get(url)
+
+    # checking request status
+    print('url request status = [{}]'.format(req_check.status_code))
+
+    if req_check.status_code == requests.codes.ok:
+        print('request OK')
+    else:
+        print('request error')
+        exit()
+
+    return
+
+
+def write_file(url_name, rows):
+    """
+    Create csv named "url_name".csv and write rows to the file
+    """
+    output_file_name = '{}.csv'.format(url_name)
+
+    print('\nWriting results to file..')
+    try:
+        with open(output_file_name, 'w', newline='') as f_output:
+            csv_output = csv.writer(f_output)
+            csv_output.writerows(rows)
+            print('file "{}" created suuccesfuly\n'.format(output_file_name))
+    except:
+        print('Error writing to file "{}" '.format(output_file_name))
+        return
+
+
+##### main ####
+def main():
+    filename = 'stocks_links_list.csv'
+
+    stocks_links_list = file_to_list(filename)
+    print(stocks_links_list)
+
+    rows = []
+    rows.append(['Symbol', '5 Day ', '1 Month', '3 Month ', 'YTD', '1 Year'])
+
+    for stock in stocks_links_list:
+        symbol = stock[0]
+        url = stock[1]
+
+        url_check(url)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        table = soup.find('table', {'class': 'table table--primary no-heading c2'})
+
+        if table is not None and len(table.find_all('li')) > 0:
+            table_elements = table.find_all('li')
+        else:
+            print('performance table for {} not found'.format(symbol))
+            continue
+
+        elements_list = []
+        elements_list.append(symbol)
+
+        for i in range(0, len(table_elements), 2):
+            try:
+                elements_list.append(str(table_elements[i].getText()))
+            except:
+                print("loading performance table of {} failed".format(symbol))
+
+        rows.append(elements_list)
+        print(elements_list)
+
+    write_file("stock performance", rows)
+
+
+main()
+

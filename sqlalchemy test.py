b90cac7db78771e0ff1ec88f0a47c76f62c57fc1
Ohad <ohadohad5@gmail.com>
2020-11-23 16:18:52 +0200

    commit 8 by Ohad
    - sqlalchemy tests fix
    - gitignore update

---
+++ sqlalchemy test.py
@@ -0,0 +1,25 @@
+import pandas
+from sqlalchemy import create_engine
+
+# filename = "C:\\Users\\liat grinberg\\Desktop\\Ohad\\ITC course\\Data_Mining_project\\Top gainers.csv"
+
+filename = "Most Active.csv"
+df = pandas.read_csv(filename)
+
+print(df)
+
+database_name = 'save_pandas'
+
+engine = create_engine('sqlite:///market_scraper.db', echo=True)
+sqlite_connection = engine.connect()
+
+sqlite_table = "Top gainers"
+df.to_sql(sqlite_table, sqlite_connection, if_exists='append')
+
+
+# with open(filename, 'r') as file:
+#     csvfile = csv.reader(file)
+#     if csvfile:
+#         df =
+
+
